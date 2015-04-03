7c0cea67ff3a0a5d706912d124d252c4b7caed4b
Paweł Sołtysiak <soltys@uznam.org>
2015-04-03 16:19:19 +0200

    git init & NGram ported

---
+++ NFranc.Tests/NGramTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NFranc.Tests
+{
+    [TestFixture]
+    public class NGramTests
+    {
+        private Func<string, IEnumerable<string>> _monoGram = NGram.CreateNGram(1);
+
+        [Test]
+        public void NegativeOrZeroThrowsRangeOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => NGram.CreateNGram(0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => NGram.CreateNGram(-1));
+        }
+
+        [Test]
+        public void MonoGramReturnsOneCharacterWhenOneCharacterGiven()
+        {
+            var unigrams = _monoGram("a").ToList();
+            Assert.AreEqual(1, unigrams.Count);
+            Assert.AreEqual("a", unigrams.ElementAt(0));
+
+
+        }
+
+        [Test]
+        public void MonoGramReturnsTwoUnigramsWhenTwoCharactersGriven()
+        {
+            var unigrams = _monoGram("ab").ToList();
+            Assert.AreEqual(2, unigrams.Count);
+            Assert.AreEqual("a", unigrams[0]);
+            Assert.AreEqual("b", unigrams[1]);
+        }
+
+        [Test]
+        public void MonoGramTest()
+        {
+            CollectionAssert.AreEqual(new[] { "t", "e", "s", "t" }, _monoGram("test"));
+            var result = string.Join("|", _monoGram("test"));
+            Assert.AreEqual("t|e|s|t", result);
+        }
+
+        [Test]
+        public void EmptyStringTest()
+        {
+            var result = _monoGram("");
+
+            Assert.AreEqual(0, result.Count());
+
+        }
+
+        [Test]
+        public void NullStringTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => _monoGram(null));
+        }
+
+        [Test]
+        public void BigramTest()
+        {
+            CollectionAssert.AreEqual(new []{"te", "es", "st"}, NGram.Bigram("test"));
+        }
+
+        [Test]
+        public void TrigramTest()
+        {
+            CollectionAssert.AreEqual(new[] { "tes", "est" }, NGram.Trigram("test"));
+        }
+
+        [Test]
+        public void TenGramTest()
+        {
+            var tenGram = NGram.CreateNGram(10);
+            CollectionAssert.AreEqual(new[] { "abcdefghij", "bcdefghijk" }, tenGram("abcdefghijk"));
+        }
+    }
+}
---
+++ NFranc/NGram.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFranc
+{
+    public class NGram
+    {
+        private static readonly Func<string, IEnumerable<string>> _bigram = CreateNGram(2);
+        public static Func<string, IEnumerable<string>> Bigram
+        {
+            get { return _bigram; }
+        }
+
+        private static readonly Func<string, IEnumerable<string>> _trigram = CreateNGram(3);
+        public static Func<string, IEnumerable<string>> Trigram
+        {
+            get { return _trigram; }
+        }
+
+
+        public static Func<string, IEnumerable<string>> CreateNGram(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentOutOfRangeException("n", "n - must be positive");
+            }
+
+            return (value) =>
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("value");
+                }
+
+                var nGrams = new Dictionary<int, string>();
+                int index = value.Length - n + 1;
+
+                if (index < 1)
+                {
+                    return nGrams.Values;
+                }
+
+                while (index-- > 0)
+                {
+                    nGrams.Add(index, value.Substring(index, n));
+                }
+                return nGrams.OrderBy(keyValue => keyValue.Key).Select(keyValue => keyValue.Value);
+            };
+        }
+    }
+}
